0f56697741cc71239cddb4b56626ac2dbe3722d7
mangoshi <soggymoose@gmail.com>
2021-10-21 14:20:56 +0100

    Styled homepage a little

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,17 +1,36 @@
 <template>
-    <div>
-        <h1>FruityCountries</h1>
-        <div class="mt-2">{{ text }}</div>
-        <b-form-input v-model="text" placeholder="Please enter the name of a country"></b-form-input>
+    <div class="bod">
+        <div class="centered">
+            <h1 v-if="text">{{ text }}</h1>
+            <h1 v-else>Fruity Countries</h1>
+            <b-form-input v-model="text" v-on:keyup.enter="searchCountry()" placeholder="Please enter the name of a country"></b-form-input>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: 'Homepage',
         data() {
             return {
                 text: ''
+            }
+        },
+        methods: {
+            searchCountry() {
+                if(!this.text) {
+                    alert("Please enter a search term!")
+                    return
+                }
+                axios
+                    .get(`https://restcountries.com/v3.1/name/${this.text}`)
+                    .then((response) => {
+                        console.log(response.data)
+                        this.country = [response.data]
+                    })
+                    .catch(error => console.log(error))
             }
         }
     }
@@ -21,5 +40,15 @@
     *{
         text-align: center;
     }
-
+    h1{
+        font-family: "Permanent Marker";
+        margin-bottom: 1em;
+    }
+    .bod{
+        height: 75vh;
+        display: grid;
+    }
+    .centered{
+        margin: auto;
+    }
 </style>
