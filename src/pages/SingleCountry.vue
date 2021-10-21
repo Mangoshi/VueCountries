6f29db7e859cfca5c8f20ed340025a5575eed608
mangoshi <soggymoose@gmail.com>
2021-10-21 12:35:34 +0100

    Fixed viewer so it returns country data

--- src/pages/SingleCountry.vue
+++ src/pages/SingleCountry.vue
@@ -9,12 +9,17 @@
 
     export default {
         name: 'SingleCountry',
+        data() {
+            return {
+                country: []
+            }
+        },
         mounted(){
             axios
                 .get(`https://restcountries.com/v3.1/name/${this.$route.params.country}?fullText=true`)
                 .then(response => {
                     console.log(response.data)
-                    this.countries = response.data})
+                    this.country = response.data[0]})
                 .catch(error => console.log(error))
         }
     }
