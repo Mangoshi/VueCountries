fb6fa4a600e52a670f3f584e5e27ea957b4caa79
mangoshi <soggymoose@gmail.com>
2021-10-26 13:56:00 +0100

    Updated country viewer page - Now shows country card and an embedded map from Google Maps. - This embedded map sits in a responsive iframe, made so using CSS.

--- src/pages/Viewer.vue
+++ src/pages/Viewer.vue
@@ -1,8 +1,46 @@
 <template>
-    <div>
-        {{ $route.params.country }}
-        
-    </div>
+    <b-container fluid>
+        <b-row cols="2" class="m-2">
+            <b-col>
+                <b-card 
+                    class="country" 
+                    header-tag="header" 
+                    footer-tag="footer"
+                    no-body
+                >
+                    <template #header>
+                        <!-- If there is a flag emoji available -->
+                        <h5 class="mb-0" v-if="country.flag">
+                            <router-link class="c_link" :to="{name: 'single_country', params: {country: country.name.official}}">{{country.flag + " " + country.name.common }}</router-link>
+                        </h5>
+                        <!-- If there is not a flag emoji available -->
+                        <h5 class="mb-0" v-else>
+                            <router-link class="c_link" :to="{name: 'single_country', params: {country: country.name.official}}">&#127937; {{country.name.common }}</router-link>
+                        </h5>
+                    </template>
+                    <b-card-body class="p-0">
+                        <b-list-group flush>
+                            <b-list-group-item class="c_capital" variant="warning" :href="'https://www.google.com/maps/place/'+country.capital"><b>Capital: </b>{{ country.capital[0] }}</b-list-group-item>
+                            <b-list-group-item class="c_region"><b>Region: </b>{{ country.region }}</b-list-group-item>
+                            <b-list-group-item class="c_subregion"><b>Subregion: </b>{{ country.subregion }}</b-list-group-item>
+                            <b-list-group-item class="c_timezone"><b>Timezone: </b>{{ country.timezones[0] }}</b-list-group-item>
+                            <b-list-group-item class="c_population"><b>Population: </b>{{ popFormat() }}</b-list-group-item>
+                        </b-list-group>
+                    </b-card-body>
+                    <template #footer>
+                        
+                    </template>
+                </b-card>
+            </b-col>
+            <b-col>
+                <b-card>
+                    <div class="iframe-rwd">
+                        <iframe scrolling="no" width="500" height="450" allowfullscreen="" loading="lazy" :src="`https://www.google.com/maps/embed/v1/place?key=AIzaSyAfD6KkQQ2eE5q5gt7lTqZBEhRZuh1FOUQ&q=${country.name.common}`"></iframe>
+                    </div>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -22,10 +60,28 @@
                     console.log(response.data)
                     this.country = response.data[0]})
                 .catch(error => console.log(error))
+        },
+        methods: {
+            popFormat(){
+                return this.country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
         }
     }
 </script>
 
 <style>
-
+    .iframe-rwd  {
+        position: relative;
+        padding-bottom: 65.25%;
+        padding-top: 30px;
+        height: 0;
+        overflow: hidden;
+    }
+    .iframe-rwd iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>
