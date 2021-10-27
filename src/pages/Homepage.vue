8258c58db3b0b11a754ee0b6ab8a816b14cf0d80
mangoshi <soggymoose@gmail.com>
2021-10-27 22:35:44 +0100

    Updated homepage search functionality (V3) - Now uses three RESTcountries endpoints at once, - then picks the first with a fulfilled response. - If all three fail, an error message displays.

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -33,21 +33,46 @@
             }
         },
         methods: {
-            searchCountry() {
+            async searchCountry() {
+                // Displaying error message if search term was empty
                 if(!this.text) {
                     alert("Please enter a search term!")
                     return
                 }
-                axios
-                    .get(`https://restcountries.com/v3.1/name/${this.text}`)
-                    .then(response => {
-                            console.log(response.data)
-                            this.countries = response.data
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        alert("Nothing was found, fucko!")
-                    })
+                // Defining requests as constants
+                const firstRequest = axios.get(`https://restcountries.com/v3.1/name/${this.text}`)
+                const secondRequest = axios.get(`https://restcountries.com/v3.1/subregion/${this.text}`)
+                const thirdRequest = axios.get(`https://restcountries.com/v3.1/currency/${this.text}`)
+                // Using Promise.allSettled instead of Promise.all so that some requests can fail
+                Promise.allSettled([firstRequest,secondRequest,thirdRequest])
+                // Using axios.spread to "spread" the requests out into an array
+                .then(axios.spread((...responses) => {
+                    // Defining responses as constants
+                    const firstResponse = responses[0]
+                    const secondResponse = responses[1]
+                    const thirdResponse = responses[2]
+                    // Logging each response
+                    console.log("First Response: ",firstResponse)
+                    console.log("Second Response: ",secondResponse)
+                    console.log("Third Response: ",thirdResponse)
+                    // Looping through the responses in the array 
+                    responses.forEach(response => {
+                        // Checking to see if the status of a response came back as fulfilled
+                        if(response.status == "fulfilled"){
+                            // If it was, log it and assign this.countries the value of its data array
+                            console.log("Fulfilled Response: ", response.value.data)
+                            this.countries = response.value.data
+                        }
+                    });
+                    // If all responses return with a status of "rejected", display an alert message and clear the search box
+                    if(responses[0].status == "rejected" && responses[1].status == "rejected" && responses[2].status == "rejected"){
+                        alert("No results... Try again!")
+                        this.text = ""
+                    }
+                }))
+                .catch(errors => {
+                    console.log('Errors detected: ', errors)
+                })
             }
         }
     }
